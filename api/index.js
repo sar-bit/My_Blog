196497b7c4f646654dc41222dcac0afa8cc9f720
sar-bit <sarbita@21twelveinteractive.com>
2022-03-14 15:04:08 +0530

    api

--- api/index.js
+++ api/index.js
@@ -5,6 +5,9 @@ const mongoose = require("mongoose");
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
 const postRoute = require("./routes/posts");
+const categoryRoute = require("./routes/categories");
+const multer = require("multer");
+
 dotenv.config();
 
 app.use(express.json());
@@ -17,9 +20,25 @@ mongoose
   .then(console.log("connected to mongodb"))
   .catch((err) => console.log("error", err));
 
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, "img.png");
+  },
+});
+
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  res.status(200).json("File has been uploaded");
+});
+
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
+app.use("/api/categories", categoryRoute);
+
 app.listen("5000", () => {
   console.log("backend listen main url");
 });
--- api/routes/posts.js
+++ api/routes/posts.js
@@ -1,5 +1,4 @@
 const router = require("express").Router();
-const User = require("../models/User");
 const Post = require("../models/Post");
 
 //CREATE NEW POST
@@ -43,22 +42,20 @@ router.put("/:id", async (req, res) => {
 //DELETE POST
 
 router.delete("/:id", async (req, res) => {
-  if (req.body.userId === req.params.id) {
-    try {
-      const user = await User.findById(req.params.id);
-
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post.username === req.body.username) {
       try {
-        await Post.deleteMany({ username: user.username });
-        await User.findByIdAndDelete(req.params.id);
-        res.status(200).json("User has beed deleted!");
+        post.delete();
+        res.status(200).json("Post has been deleted!");
       } catch (err) {
         res.status(500).json(err);
       }
-    } catch (e) {
-      res.status(404).json("User not found!");
+    } else {
+      res.status(401).json("You can delete only your post!");
     }
-  } else {
-    res.status(401).json("You can delete only your account!");
+  } catch (e) {
+    res.status(500).json(err);
   }
 });
 
@@ -66,9 +63,32 @@ router.delete("/:id", async (req, res) => {
 
 router.get("/:id", async (req, res) => {
   try {
-    const user = await User.findById(req.params.id);
-    const { password, ...others } = user._doc;
-    res.status(200).json(others);
+    const post = await Post.findById(req.params.id);
+    res.status(200).json(post);
+  } catch (e) {
+    res.status(500).json(err);
+  }
+});
+
+//GET ALL POST
+
+router.get("/", async (req, res) => {
+  const username = req.query.user;
+  const catName = req.query.cat;
+  try {
+    let posts;
+    if (username) {
+      posts = await Post.find({ username });
+    } else if (catName) {
+      posts = await Post.find({
+        categories: {
+          $in: [catName],
+        },
+      });
+    }else{
+      posts = await Post.find();
+    }
+    res.status(200).json(posts);
   } catch (e) {
     res.status(500).json(err);
   }
